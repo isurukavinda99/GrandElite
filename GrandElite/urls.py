3a2067a9cb1ec2c59ef984e5b4c98c9b51dfe6c1
isurukavinda99 <isurukavinda999@gmail.com>
2021-08-09 00:02:08 +0530

    Grand Elite Initial Commit

---
+++ GrandElite/urls.py
@@ -0,0 +1,22 @@
+
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.urls import path , include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    #shalitha
+    #kavinda
+    #himasha
+    #mihara
+
+    path('demo' , include('demo.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
