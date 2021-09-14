9dce6ea6e89ce3839fa1c9b1db37993d5f59451a
Himasha-97 <himashak97@gmail.com>
2021-09-14 15:13:52 +0530

    registered a new model of supplier payments part

--- supplierPayemts/admin.py
+++ supplierPayemts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from supplierPayemts.models import SupplierDetails
+
+admin.site.register(SupplierDetails)
--- supplierPayemts/models.py
+++ supplierPayemts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class SupplierDetails(models.Model):
+    supName = models.CharField(max_length=50)
+    supEmail = models.EmailField()
+    supPhone = models.CharField(max_length=10)
+    product = models.CharField(max_length=50)
+    amount = models.FloatField()
