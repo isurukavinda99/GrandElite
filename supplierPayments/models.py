55297ae7585083c7da6eb524a77898c11f704771
Himasha-97 <himashak97@gmail.com>
2021-09-15 11:52:19 +0530

    merged isms branch into pms

--- supplierPayments/models.py
+++ supplierPayments/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 # Create your models here.
 
-class SupplierDetails(models.Model):
-    supName = models.CharField(max_length=50)
-    supEmail = models.EmailField()
-    supPhone = models.CharField(max_length=10)
-    product = models.CharField(max_length=50)
-    amount = models.FloatField()
-    status = [
-        ('draft', 'Draft'),
-        ('paid', 'Paid')
-    ]
+# class SupplierDetails(models.Model):
+#     supName = models.CharField(max_length=50)
+#     supEmail = models.EmailField()
+#     supPhone = models.CharField(max_length=10)
+#     product = models.CharField(max_length=50)
+#     amount = models.FloatField()
+#     status = [
+#         ('draft', 'Draft'),
+#         ('paid', 'Paid')
+#     ]
 
